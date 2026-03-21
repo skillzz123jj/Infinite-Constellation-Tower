3a2f387d0c85f4d0f11823b6a0091716e6728a21
Mila <turpeinenmila6@gmail.com>
2026-03-21 21:09:48 +0200

    Add left/right movement, jump and new input syetem

---
+++ Assets/Scripts/MovementScripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float speed = 8f;
+    [SerializeField] float jumpForce = 5f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+    [SerializeField] bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    public void Move(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed && isGrounded)
+        {
+            Debug.Log("jump");
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+    private void FixedUpdate()
+    {
+        Vector2 velocity = rb.linearVelocity;
+        velocity.x = moveInput.x * speed;
+        rb.linearVelocity = velocity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+}
